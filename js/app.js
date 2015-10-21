ceee03d488bfee86c49f922c61a959a748fc4709
ABracken <adriannbracken214@gmail.com>
2015-10-21 08:08:13 -0700

    Modal done

--- js/app.js
+++ js/app.js
@@ -11,18 +11,28 @@ function MyViewModel() {
 		});
 	};
 	
-	self.displayDetails = ko.observable();
+	self.displayDetails = {
+		Title: ko.observable(),
+		Year: ko.observable(),
+		Rated: ko.observable(),
+		Poster: ko.observable(),
+		Released: ko.observable(),
+		Runtime: ko.observable(),
+		Genre: ko.observable(),
+		Director: ko.observable(),
+		Writer: ko.observable(),
+		Actors: ko.observable(),
+		Plot: ko.observable(),
+		Language: ko.observable(),
+		Country: ko.observable(),
+		Awards: ko.observable(),
+		imdbRating: ko.observable(),	
+	};
 	self.searchDisplay = function(movie) {
-		$.getJSON("http://www.omdbapi.com/?i=" + self.imdbID, function(data){
-			ko.mapping.fromJS(data.Search, {}, self.displayDetails)
+		$.getJSON("http://www.omdbapi.com/?i=" + movie.imdbID(), function(data){
+			ko.mapping.fromJS(data, {}, self.displayDetails)
 		});
 	};
-	/* self.imdbID = ko.observable();
-	self.displayDetails = ko.observable();
-	
-	self.searchDisplay = function() {
-		
-	}; 
-	*/
+
 };
 ko.applyBindings(new MyViewModel());
